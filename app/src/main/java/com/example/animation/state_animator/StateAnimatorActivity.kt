bf53f1e875bbee426030aebeb49cef38357cebe6
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-25 13:39:48 +0300

    StateAnimatorDemo

---
+++ app/src/main/java/com/example/animation/state_animator/StateAnimatorActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animation.state_animator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.animation.R
+
+class StateAnimatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_animator)
+    }
+}
