c6afd3d62455f3cf3bf3d7d6e3874daecff6abec
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-25 13:07:11 +0300

    AnimationDrawable demo

---
+++ app/src/main/java/com/example/animation/animation_list/AnimationListActivity.kt
@@ -0,0 +1,18 @@
+package com.example.animation.animation_list
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animation.R
+import kotlinx.android.synthetic.main.activity_animation_list.*
+
+class AnimationListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation_list)
+
+        val animationDrawable = ivAnimation.drawable as AnimationDrawable
+        animationDrawable.start()
+    }
+}
