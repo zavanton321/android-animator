0824708ae97432ff16cb974006c1e5a0d9f6a1f0
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-25 14:17:05 +0300

    StateListAnimator

--- app/src/main/java/com/example/animation/state_list_animator/StateListAnimatorActivity.kt
+++ app/src/main/java/com/example/animation/state_list_animator/StateListAnimatorActivity.kt
@@ -1,10 +1,10 @@
-package com.example.animation.state_animator
+package com.example.animation.state_list_animator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.animation.R
 
-class StateAnimatorActivity : AppCompatActivity() {
+class StateListAnimatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
